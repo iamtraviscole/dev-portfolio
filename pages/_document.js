cde6404ade8abae2f76a92a037507ae6f51385a5
Travis Cole <iamtraviscole@gmail.com>
2021-06-30 05:02:02 -0400

    add google fonts

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link 
+            // 'By default, Next.js will automatically inline font CSS at build time...'
+            // https://nextjs.org/docs/basic-features/font-optimization
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@700&family=IBM+Plex+Mono:wght@300;400;500;600;700" 
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
