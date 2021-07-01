bc10b8dadeb8e55f67581579dd1e01a6fee4d6c1
Travis Cole <iamtraviscole@gmail.com>
2021-07-01 17:42:39 -0400

    add Head to layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,32 @@
 import React from 'react'
+import Head from 'next/head'
+
 import Nav from './Nav'
 import Footer from './Footer'
 
 import styles from '../styles/components/Layout.module.scss'
 
-export default function Layout({ children }) {
+export default function Layout(props) {
   return (
+    <> 
+    <Head>
+      <title>{props.title || 'Travis Cole - Web Development'}</title>
+      <meta name="description" content={props.description} />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
     <div className={styles.grid}>
       <header>
         <Nav />
       </header>
       <main>
-        content
-        {children}
+        <div className={styles.container}>
+          {props.children}
+        </div>
       </main>
       <footer>
         <Footer />
       </footer>
     </div>
+    </>
   )
 }
