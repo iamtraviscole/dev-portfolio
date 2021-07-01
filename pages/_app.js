3d82f54127e8b16bc4a2e1f33fe8100cafeb6b00
Travis Cole <iamtraviscole@gmail.com>
2021-07-01 19:58:02 -0400

    build and style Index

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import '../styles/globals/reset.scss'
 import '../styles/globals/theme.scss'
 
+import '../styles/pages/Index.scss'
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,33 @@
-import Head from 'next/head'
+import React from 'react'
+
 import Layout from '../components/Layout'
 
-export default function Home() {
+import Smile from '../public/icons/smile.svg'
+
+export default function Index() {
   return (
-    <>
-      <Layout>
-        <Head>
-          <title>Travis Cole</title>
-          <meta name="description" content="" />
-          <link rel="icon" href="/favicon.ico" />
-        </Head>
-      </Layout>
-    </>
+    <Layout
+      title='Travis Cole - Web Development'
+      description=''
+    >
+      <div className='Index'>
+        <div className='Index__intro'>
+          <h1 className='Index__intro-header'>
+            Front end focused full stack developer and designer.
+            <span className='Index__intro-header-smiley'><Smile /></span>
+          </h1>
+        </div>
+        <div className='Index__skills'>
+          <ul className='Index__skills-list'>
+            <li>JavaScript</li>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>React</li>
+            <li>Node.js</li>
+            <li>Ruby</li>
+          </ul>
+        </div>
+      </div>
+    </Layout>
   )
 }
