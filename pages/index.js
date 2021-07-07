455795411946fba746c031c8c7b80eedef2b6421
Travis Cole <iamtraviscole@gmail.com>
2021-07-07 00:53:21 -0400

    update styles, add highlight, icons, vertically center

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import React from 'react'
 import Layout from '../components/Layout'
 
 import SmileIcon from '../public/icons/smile.svg'
+import JavaScriptIcon from '../public/icons/javascript.svg'
+import ReactIcon from '../public/icons/react.svg'
+import HTML5Icon from '../public/icons/html5.svg'
+import CSS3Icon from '../public/icons/css3.svg'
 
 export default function Index() {
   return (
@@ -13,18 +17,16 @@ export default function Index() {
       <div className='Index'>
         <div className='Index__intro'>
           <h1>
-            Front end focused full stack developer and designer.
-            <span><SmileIcon /></span>
+            <span className='Index__intro-highlight'>Front end</span> focused full stack developer and designer.
+            <span className='Index__intro-icon'><SmileIcon /></span>
           </h1>
         </div>
         <div className='Index__skills'>
           <ul>
-            <li>JavaScript</li>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>React</li>
-            <li>Node.js</li>
-            <li>Ruby</li>
+            <li><JavaScriptIcon />JavaScript</li>
+            <li><ReactIcon />React</li>
+            <li><HTML5Icon />HTML</li>
+            <li><CSS3Icon />CSS</li>
           </ul>
         </div>
       </div>
