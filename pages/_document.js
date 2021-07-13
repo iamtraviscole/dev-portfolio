b282d98ee9f9436e55931489ec54d2d3a51a0a3c
Travis Cole <iamtraviscole@gmail.com>
2021-07-13 19:42:00 -0400

    add weight and font

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link 
             // 'By default, Next.js will automatically inline font CSS at build time...'
             // https://nextjs.org/docs/basic-features/font-optimization
-            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Poppins:wght@700'
+            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=Poppins:wght@700&family=Syne+Mono'
             rel='stylesheet'
           />
         </Head>
