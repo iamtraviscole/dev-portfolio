4358029bc169e681cfec3a2307eb1ddbabf5acf2
Travis Cole <iamtraviscole@gmail.com>
2021-07-14 20:04:17 -0400

    change particle and add corners

--- components/PageHeader.js
+++ components/PageHeader.js
@@ -4,10 +4,16 @@ export default function PageHeader(props) {
   return (
     <div className='PageHeader'>
       <div className='PageHeader__header'>
-        <span className='PageHeader__header-particle'></span>
+        <div className='PageHeader__header-particle'>&there4;</div>
         <div className='PageHeader__header-heading'>
+          <div className='PageHeader__header-heading-corners'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
           <h1>{props.heading}</h1>
-          {props.icon ? <span>{<props.icon />}</span> : ''}
+          {props.icon && <span>{<props.icon />}</span>}
         </div>
       </div>
     </div>
