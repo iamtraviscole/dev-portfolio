dd88bbb0bcdd7479461dad57a2ed26570cf5882d
Travis Cole <iamtraviscole@gmail.com>
2021-08-20 15:29:06 -0400

    add logo

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
 
+import Logo from '../public/logo.svg'
+
 export default function Nav() {
   return (
       <nav className='Nav'>
         <div className='Nav__logo'>
-          <Link href='/'><a>&gt; travis cole</a></Link>
+          <Link href='/'><a><Logo /></a></Link>
         </div>
         <ul className='Nav__links'>
           <li><Link href='/skills'><a>Skills</a></Link></li>
