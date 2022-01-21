ea6b60bc9afd770a564c15e856ff76df1d1154a6
Travis Cole <iamtraviscole@gmail.com>
2022-01-20 21:49:37 -0500

    update footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import Link from 'next/link'
+
+import GithubIcon from '../public/icons/github.svg'
+import TwitterIcon from '../public/icons/twitter.svg'
 
 export default function Footer() {
   return (
     <div className='Footer'>
       <ul>
-        <li>Lorem</li>
-        <li>Excepturi</li>
-        <li>Facilis</li>
-        <li>Ratione</li>
+        <li>&copy; 2022 Travis Cole</li>
+        {/* TODO: add links */}
+        <li><Link href='http://github.com'><a target='_blank'><GithubIcon /></a></Link></li>
+        <li><Link href='http://twitter.com'><a target='_blank'><TwitterIcon /></a></Link></li>
       </ul>
     </div>
   )
