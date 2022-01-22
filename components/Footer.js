cad542b97c6dcc02beb6c3953e3952f6ce46a60c
Travis Cole <iamtraviscole@gmail.com>
2022-01-21 23:05:21 -0500

    update style, make year dynamic

--- components/Footer.js
+++ components/Footer.js
@@ -5,14 +5,20 @@ import GithubIcon from '../public/icons/github.svg'
 import TwitterIcon from '../public/icons/twitter.svg'
 
 export default function Footer() {
+  const year = new Date().getFullYear()
+
   return (
     <div className='Footer'>
-      <ul>
-        <li>&copy; 2022 Travis Cole</li>
-        {/* TODO: add links */}
-        <li><Link href='http://github.com'><a target='_blank'><GithubIcon /></a></Link></li>
-        <li><Link href='http://twitter.com'><a target='_blank'><TwitterIcon /></a></Link></li>
-      </ul>
+      <div className='Footer__socials'>
+        <ul>
+          {/* TODO: add links */}
+          <li><Link href='http://github.com'><a target='_blank'><GithubIcon /></a></Link></li>
+          <li><Link href='http://twitter.com'><a target='_blank'><TwitterIcon /></a></Link></li>
+        </ul>
+      </div>
+      <div className='Footer__copyright'>
+        <p>&copy; {year} Travis Cole</p>
+      </div>
     </div>
   )
 }
