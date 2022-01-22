4c448282ca6ef7dfe641c61b52545bb747929fc8
Travis Cole <iamtraviscole@gmail.com>
2022-01-22 18:36:55 -0500

    remove contact link

--- components/Nav.js
+++ components/Nav.js
@@ -13,7 +13,6 @@ export default function Nav() {
           <li><Link href='/skillset'><a>Skillset</a></Link></li>
           <li><Link href='/projects'><a>Projects</a></Link></li>
           <li><Link href='/about'><a>About</a></Link></li>
-          <li><Link href='/contact'><a>Contact</a></Link></li>
         </ul>
       </nav>
   )
