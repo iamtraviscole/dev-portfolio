c61f86a3362a0ce8dec93dc64ff12c276510b810
Travis Cole <iamtraviscole@gmail.com>
2022-01-23 22:14:33 -0500

    remove unneeded React imports

--- components/Footer.js
+++ components/Footer.js
@@ -1,4 +1,3 @@
-import React from 'react'
 import Link from 'next/link'
 
 import GithubIcon from '../public/icons/github.svg'
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,3 @@
-import React from 'react'
-
 import Layout from '../components/Layout'
 
 import JavaScriptIcon from '../public/icons/javascript.svg'
