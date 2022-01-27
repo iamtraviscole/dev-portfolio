994915c16a9208f28e73169aa033151762be6e22
Travis Cole <iamtraviscole@gmail.com>
2022-01-27 01:15:54 -0500

    increase h2-h6 default font weight

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ class MyDocument extends Document {
           <link 
             // 'By default, Next.js will automatically inline font CSS at build time...'
             // https://nextjs.org/docs/basic-features/font-optimization
-            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;600&family=Poppins:wght@700&family=VT323'
+            href='https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@400;700&family=Poppins:wght@700&family=VT323'
             rel='stylesheet'
           />
           <meta name='viewport' content='width=device-width initial-scale=1' />
